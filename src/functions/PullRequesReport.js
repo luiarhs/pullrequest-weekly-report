be3be7820d9df45c7d36945745dd6cd59b3054a1
luiarhs <luiarhs@yandex.com>
2024-03-10 02:22:49 -0600

    [add] search query to get pull request

--- src/functions/PullRequesReport.js
+++ src/functions/PullRequesReport.js
@@ -4,38 +4,52 @@ const { Octokit } = require("@octokit/core");
 app.http("PullRequestReport", {
 	methods: ["POST"],
 	authLevel: "anonymous",
+    route: 'report/',
 	handler: async (request, context) => {
 		context.log(`Http function processed request for url "${request.url}"`);
 
-		const octokit = new Octokit({
-			headers: {
-				authorization: `token ${process.env.GITHUB_TOKEN}`
-			}
-		});
+		//TODO: Validate email address and send email
+		const octokit = new Octokit();
 
 		try {
-            // TODO: Make variables configurable as function input or environment variables
-			const owner = "luiarhs";
-			const repo = "sailpoint-challenge";
-			const branch = "main";
-            
-            const data = await octokit.graphql({
-                headers: {
-                    accept: 'application/vnd.github+json',
-                    authorization: `token ${pat}`
-                },
-                query: `query  Repository {
-                    repository(owner: "${owner}", name: "${repo}") {
-                        ref(qualifiedName: "${branch}") {
-                            target {
-                                ... on Commit {
-                                    oid
-                                }
-                            }
-                        }
-                    }
-                }`
-            });
+			// TODO: Make variables configurable as function input or environment variables
+			const owner = "brave";
+			const repo = "security-action";
+
+			// Filter pull requests created in the last week
+			const lastWeek = new Date();
+			lastWeek.setDate(lastWeek.getDate() - 7);
+
+			const data = await octokit.graphql({
+				headers: {
+					accept: 'application/vnd.github+json',
+					authorization: `token ${process.env.GITHUB_TOKEN}`
+				},
+				query: `query Search {
+					search(
+						query: "repo:${owner}/${repo} type:pr created:>${lastWeek.toISOString()}",
+						type: ISSUE
+						first: 100
+					) {
+						issueCount
+						nodes {
+							... on PullRequest {
+								author {
+									login
+									url
+								}
+								baseRefName
+								createdAt
+								isDraft
+								permalink
+								state
+								title
+								url
+							}
+						}
+					}
+				}`
+			});
 
 			context.log(data);
 		} catch (error) {
