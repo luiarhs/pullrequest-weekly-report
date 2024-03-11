8aaf139d94cd2d364e5b2cbe9cafa60ecc1b9e51
luiarhs <luiarhs@yandex.com>
2024-03-11 13:12:02 -0600

    [add] templates

--- src/templates/header.js
+++ src/templates/header.js
@@ -2,7 +2,6 @@
 
 let header = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
-
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
@@ -110,7 +109,6 @@ let header = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://w
     }
   </style>
 </head>
-
 <body>
   <center class="wrapper" data-link-color="#1188E6"
     data-body-style="font-size:14px; font-family:inherit; color:#000000; background-color:#f0f0f0;">
@@ -124,24 +122,18 @@ let header = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://w
                   <table width="100%" bgcolor="#cc26b0" cellpadding="0" cellspacing="0" border="0">
                     <tr>
                       <td style="padding:16px 32px; line-height:40px; text-align:inherit;">
-                        <h1>Weekly Summary ${data.name}</h1>
+                        <h1 style="color: #0033a0">Weekly Summary</h1>
                         <h3 style="color: #ffffff;">Here is a summary of the pull requests that were created in the last week.</h3>
                       </td>
                     </tr>
-                    
                   </table>
                   <table class="divider-content" role="content-container" cellspacing="0" cellpadding="0" border="0" width="100%">
-                    <tr style="color: #415364;">
+                    <tr style="color: #0033a0;">
                       <td style="padding:10px;"><strong>Title</strong></td>
                       <td style="padding:10px;"><strong>Author</strong></td>
                       <td style="padding:10px;"><strong>State</strong></td>
                       <td style="padding:10px;"><strong>Created At</strong></td>
-                      <td style="padding:10px;"><strong>View it on GitHub</strong></td>
-                    </tr>
-                  </table>
-                  <tr>
-                    <td style="padding:0px 0px 3px 0px;" bgcolor="#cc26b0"></td>
-                  </tr>
-                  <table class="divider-content" role="content-container" cellspacing="0" cellpadding="0" border="0" width="100%">`;
+                      <td style="padding:10px;"><strong>Link</strong></td>
+                    </tr>`;
 
 module.exports = header;
